00df773284b174781a520d69b4f4fa0c6769a90e
Thomas Carr <9591402+htcarr3@users.noreply.github.com>
2023-03-31 22:40:15 -0400

    adding ethr class

---
+++ lib/didx/ethr.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# class DIDX::Ethr is a class that represents a DID Ethr.
+# It follows the did:ethr specification:
+# https://github.com/decentralized-identity/ethr-did-resolver/blob/master/doc/did-method-spec.md
+module DIDX
+  class Ethr
+
+    def resolve; end
+
+    def parse_method_id; end
+  end
+end
