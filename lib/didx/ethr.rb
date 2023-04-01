cc17b8c86b70cc9eacb2ca3dbba27b29cbf256dc
Thomas Carr <9591402+htcarr3@users.noreply.github.com>
2023-03-31 23:24:40 -0400

    contract registry

--- lib/didx/ethr.rb
+++ lib/didx/ethr.rb
@@ -5,8 +5,14 @@
 # https://github.com/decentralized-identity/ethr-did-resolver/blob/master/doc/did-method-spec.md
 module DIDX
   class Ethr < Generic
+    CONTRACTS = {
+      mainnet: "0xdca7ef03e98e0dc2b855be647c39abe984fcf21b"
+    }.freeze
+
     attr_reader :eth_address
-    def resolve; end
+    def resolve
+      contract = CONTRACTS[:mainnet]
+    end
 
     def parse_method_id
       @eth_address = @method_id
