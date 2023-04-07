ec3e64cd67cfb7c2324065c3feb7f65cf16a81dd
Thomas Carr <9591402+htcarr3@users.noreply.github.com>
2023-04-07 09:16:37 -0400

    adding some eth client api calls

--- lib/didx/ethr.rb
+++ lib/didx/ethr.rb
@@ -1,14 +1,28 @@
 # frozen_string_literal: true
 
+require "eth"
 # class DIDX::Ethr is a class that represents a DID Ethr.
 # It follows the did:ethr specification:
 # https://github.com/decentralized-identity/ethr-did-resolver/blob/master/doc/did-method-spec.md
 module DIDX
   class Ethr < Generic
-    CONTRACT_ADDRESS = "0xdca7ef03e98e0dc2b855be647c39abe984fcf21b".freeze
+    CONTRACT_ADDRESS = "0xdca7ef03e98e0dc2b855be647c39abe984fcf21b"
+    NETWORKS = %i[mainnet polygon ropsten rinkeby kovan goerli mumbai].freeze
+    ALCHEMY_API = "https://eth-mainnet.g.alchemy.com/v2/#{ENV["ALCHEMY_API_KEY"]}"
+    INFURA_API = "https://mainnet.infura.io/v3/#{ENV["INFURA_TOKEN"]}"
     attr_reader :eth_address
+
+    def client
+      @client ||= Eth::Client.create(INFURA_API)
+    end
+
+    def eth_balance
+      client.get_balance(@eth_address)
+    end
+
+    private
     def resolve
-      contract = CONTRACT_ADDRESS
+
     end
 
     def parse_method_id
